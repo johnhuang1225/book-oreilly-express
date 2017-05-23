b8495613a317ddc90643b0aea345bbfc3d6f6616
John Huang <johnhuang1225@gmail.com>
2017-05-23 10:17:24 +0800

    Initial commit

---
+++ ch03/meadowlark.js
@@ -0,0 +1,39 @@
+/**
+ * Created by a0801 on 2017/5/23.
+ */
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars')
+                .create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
+app.use(function(req, res, next){
+    // res.type('text/plain');
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(error, req, res, next){
+    console.log(error.stack);
+    // res.type('text/plain');
+    res.status(500);
+    res.render('505');
+});
+
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate');
+});
+
+
