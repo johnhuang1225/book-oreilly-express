352b9a764aac0c8e7778131c65b9aa96ba4c91bb
John Huang <johnhuang1225@gmail.com>
2017-05-23 13:31:46 +0800

    add download file router

--- ch03/meadowlark.js
+++ ch03/meadowlark.js
@@ -1,6 +1,7 @@
 /**
  * Created by a0801 on 2017/5/23.
  */
+var path = require('path');
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars')
@@ -11,8 +12,10 @@ app.set('view engine', 'handlebars');
 var names = require('./lib/names');
 
 app.set('port', process.env.PORT || 3000);
-
 app.use(express.static(__dirname + '/public'));
+
+// app.disable('x-powered-by'); // 設定不顯示 response header 的 x-powered-by
+
 
 app.get('/', function(req, res){
     res.render('home');
@@ -21,6 +24,27 @@ app.get('/about', function(req, res){
     var name = names.getRandomName();
     res.render('about', {name: name});
 });
+app.get('/headers', function(req, res){
+    res.set('Conttent-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) {
+        s += name + ': ' + req.headers[name] + '<br>';
+    }
+    res.send(s);
+});
+
+//下載檔案
+app.get('/attachment', function(req, res){
+    console.log('file:' + path.join(__dirname, '/public/img/nodejs.png'));
+    res.download(path.join(__dirname, '/public/img/nodejs.png'), 'node.png', function(err){
+        if (err) {
+            console.log('error:' + err.stack);
+        } else {
+            console.log('success');
+        }
+    });
+});
+
 
 app.use(function(req, res, next){
     // res.type('text/plain');
